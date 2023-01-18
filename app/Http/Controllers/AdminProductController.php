0dff7753395baa1693ac0017662585906e7b99af
Ítalo Sátiro <italosatiro2@gmail.com>
2023-01-18 17:49:00 -0300

    first commit

---
+++ app/Http/Controllers/AdminProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminProductController extends Controller
+{
+    public function index(){
+        return view('admin.products');
+    }
+
+    public function edit(){
+        return view('admin.productEdit');
+    }
+}
